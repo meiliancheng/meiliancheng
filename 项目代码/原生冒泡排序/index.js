1bb2517e200b68319f2a5d00e096def64f4e1bce
Mlc123 <1427391913@qq.com>
2021-05-21 14:27:23 +0800

    meilianchneg

--- 项目代码/原生冒泡排序/index.js
+++ 项目代码/原生冒泡排序/index.js
@@ -1,10 +1,14 @@
 function unEevey(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 1; j < arr.length - 1; j++) {
-            if(arr[i]===arr[j]{
-				arr.splice(i,1)
-				i - -
-		}
+    let temp;
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            // console.log(arr[i] + " ........" + arr[j]);
+            if (arr[j] > arr[j + 1]) {
+                console.log(arr[j] + " ........" + arr[j + 1]);
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
     return arr;
